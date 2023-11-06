f7d256f391a32094e977b77c4b0ab3f96055f44d
INJAM UL HAQUE <injamtanvir@gmail.com>
2023-11-06 21:24:11 +0600

    update all

--- Menu/INLATAN.IO/script.js
+++ Menu/INLATAN.IO/script.js
@@ -1,3 +1,15 @@
-function redirect(url) {
-    window.location.href = url;
-}
+document.addEventListener('DOMContentLoaded', (event) => {
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => {
+        box.addEventListener('click', function() {
+            const id = this.id;
+            // Update with actual paths
+            const paths = {
+                'explore-space': 'INDIGO/ExploreSpace/dashboardvideo.html',
+                'birthday-wish': 'INDIGO/BirthdayWish/inlatan.html',
+                'my-card': 'INDIGO/MyCard/index.html'
+            };
+            window.location.href = paths[id] || '#';
+        });
+    });
+});
