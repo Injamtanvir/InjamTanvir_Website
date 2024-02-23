d56d8b98825afc90fc37ef6f1df73010ad285a29
INJAM UL HAQUE <111838181+Injamtanvir@users.noreply.github.com>
2024-02-24 01:18:28 +0600

    Update script.js

--- Sign-In/script.js
+++ Sign-In/script.js
@@ -1,41 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var signup = document.querySelector('.signup');
-    var signin = document.querySelector('.signin');
-    var signUpScreen = document.querySelector('.signup-screen');
-    var signInScreen = document.querySelector('.signin-screen');
-    var a = false;
-
-    signup.addEventListener('click', function(event) {
-        event.preventDefault(); // Prevent default form submission behavior
-        // Toggle between sign-up and sign-in screens
-        toggleScreens();
-    });
-
-    signin.addEventListener('click', function(event) {
-        event.preventDefault(); // Prevent default form submission behavior
-        // Toggle between sign-up and sign-in screens
-        toggleScreens();
-    });
-
-    var toggleScreens = function() {
-        signup.classList.toggle('option-toggle');
-        signin.classList.toggle('option-toggle');
-        signInScreen.classList.toggle('non-display');
-        signUpScreen.classList.toggle('non-display');
-        a = !a;
-    };
-
-    var handleSignup = function() {
-        // Redirects to the menu page after sign up
-        window.location.href = '/Menu/menu.html';
-    };
-
-    var handleSignin = function() {
-        // Redirects to the menu page after sign in
-        window.location.href = '/Menu/menu.html';
-    };
-
-    // Adding event listeners to sign-up and sign-in buttons
-    document.getElementById('signup-form').addEventListener('submit', handleSignup);
-    document.getElementById('signin-form').addEventListener('submit', handleSignin);
-});
+function toggleForms() {
+    var signupForm = document.getElementById('signup-form');
+    var signinForm = document.getElementById('signin-form');
+    if (signupForm.style.display === 'none') {
+      signupForm.style.display = 'block';
+      signinForm.style.display = 'none';
+    } else {
+      signupForm.style.display = 'none';
+      signinForm.style.display = 'block';
+    }
+  }
+  
+  
+  function handleSignup() {
+    // Here, you can implement any front-end validation or operations you need before redirection
+    window.location.href = '/Menu/menu.html'; // Redirects to the Dashboard after sign up
+  }
+  
+  function handleSignin() {
+    // Here, you can implement any front-end validation or operations you need before redirection
+    window.location.href = '/Menu/menu.html'; // Redirects to the Dashboard after sign in
+  }
