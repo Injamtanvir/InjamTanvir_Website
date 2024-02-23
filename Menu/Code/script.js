0789daa2af6f26a25a43a2c6681c5422ac75f4d8
INJAM UL HAQUE <111838181+Injamtanvir@users.noreply.github.com>
2024-02-24 01:02:45 +0600

    Update script.js

--- Menu/Code/script.js
+++ Menu/Code/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const targetDate = new Date("January 1, 2024 00:00:00").getTime();
+    const targetDate = new Date("June 1, 2024 00:00:00").getTime();
 
     const timerInterval = setInterval(function() {
         const now = new Date().getTime();
